60c6a5b1c66bd777c0caa5de3b6a48ece5f9bdfe
y.rabeshka <y.rabeshka.vironit@vironit.ru>
2019-08-16 12:24:18 +0300

    doing ajax pagination

--- IdentityStore/Controllers/HomeController.cs
+++ IdentityStore/Controllers/HomeController.cs
@@ -23,8 +23,6 @@ namespace IdentityStore.Controllers
         // GET: Product
         public ActionResult Index(int? page)
         {
-
-
             var pr = service.GetAllProducts();
             List<ProductViewModel> viewModels = new List<ProductViewModel>();
             foreach (var e in pr)
@@ -35,7 +33,19 @@ namespace IdentityStore.Controllers
             int pageNumber = (page ?? 1);
             return View(viewModels.ToPagedList(pageNumber, pageSize));
         }
-
+        [HttpGet]
+        public ActionResult Products(int? page)
+        {
+            var pr = service.GetAllProducts();
+            List<ProductViewModel> viewModels = new List<ProductViewModel>();
+            foreach (var e in pr)
+            {
+                viewModels.Add(new ProductViewModel { Id = e.Id, Category = new CategoryViewModel { Id = e.Category.Id, Name = e.Category.Name }, Description = e.Description, Name = e.Name, Price = e.Price });
+            }
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return PartialView(viewModels.ToPagedList(pageNumber, pageSize));
+        }
         [HttpGet]
         public ActionResult AddToCart(int? id)
         {
